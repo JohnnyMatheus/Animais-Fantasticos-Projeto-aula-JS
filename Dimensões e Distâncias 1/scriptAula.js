034126d608610d5caa505b644023bde7ec6c24b1
JohnnyMatheus <alunotioficial@gmail.com>
2026-01-03 20:48:43 -0300

    estudando

---
+++ Dimensões e Distâncias 1/scriptAula.js
@@ -0,0 +1,12 @@
+const listaAnimais = document.querySelector('.animais-lista');
+
+const height = listaAnimais.scrollHeight;
+console.log(height)
+
+const animaisTop = listaAnimais.offsetTop;
+console.log(animaisTop)
+
+
+const primeiroH2 = document.querySelector('h2');
+const h2left = primeiroH2.offsetLeft;
+console.log(animaisTop);
