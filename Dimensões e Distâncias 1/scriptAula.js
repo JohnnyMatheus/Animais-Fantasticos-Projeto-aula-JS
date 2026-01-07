710007ea9ad391aaa4755527ff436cc0474d04e8
JohnnyMatheus <alunotioficial@gmail.com>
2026-01-07 01:41:37 -0300

    Update scriptAula.js

--- Dimensões e Distâncias 1/scriptAula.js
+++ Dimensões e Distâncias 1/scriptAula.js
@@ -1,12 +1,39 @@
-const listaAnimais = document.querySelector('.animais-lista');
+// const listaAnimais = document.querySelector('.animais-lista');
 
-const height = listaAnimais.scrollHeight;
-console.log(height)
+// const height = listaAnimais.scrollHeight;
+// console.log(height);
 
-const animaisTop = listaAnimais.offsetTop;
-console.log(animaisTop)
+// const width = listaAnimais.scrollWidth;
+// console.log(width);
+
+// const animaisTop = listaAnimais.offsetTop;
+// console.log(animaisTop);
 
 
-const primeiroH2 = document.querySelector('h2');
-const h2left = primeiroH2.offsetLeft;
-console.log(animaisTop);
+// const primeiroh2 = document.querySelector('h2');
+// const h2left = primeiroh2.offsetLeft;
+// console.log(h2left);
+
+
+// const rect = primeiroh2.getBoundingClientRect();
+// console.log(rect)
+// console.log(rect.top);
+
+
+
+window.innerWidth; // width do janela
+window.outerWidth; // soma dev tools também
+window.innerHeight; // height do janela
+window.outerHeight; // soma a barra de endereço
+
+window.pageYOffset; // distância total do scroll vertical
+window.pageXOffset; // distância total do scroll horizontal
+
+if(window.innerWidth < 600) {
+  console.log('Tela menor que 600px');
+}
+
+
+const small = window.matchMedia('(max-width: 600px');
+
+
